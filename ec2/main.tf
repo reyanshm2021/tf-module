8d8f2f3e9823e44b53feae50bcbe6a1ca455378c
rakeshmaurya81 <rakesh.maurya@opstree.com>
2024-12-18 19:09:28 +0530

    wip

---
+++ ec2/main.tf
@@ -0,0 +1,19 @@
+resource "aws_instance" "ec2" {
+  count                       = var.count_ec2_instance
+  ami                         = var.ami_id
+  instance_type               = var.instance_type
+  associate_public_ip_address = var.public_ip
+  key_name                    = var.key_name
+  #subnet_id                   = var.subnet
+  #vpc_security_group_ids      = var.security_groups
+
+  tags = merge(
+    {
+      Name = format("%s-%d", var.name, count.index + 1)
+    },
+    {
+      PROVISIONER = "Terraform"
+    },
+    var.tags,
+  )
+}
---
+++ ec2/variable.tf
@@ -0,0 +1,87 @@
+variable "count_ec2_instance" {
+  description = "number of ec2 instance"
+  type        = number
+  default     = 1
+}
+
+variable "name" {
+  description = "Name of bastion"
+  type        = string
+  default     = ""
+}
+variable "public_ip" {
+  description = "Public Ip "
+  type        = bool
+  default     = false
+}
+
+variable "tags" {
+  description = "Additional tags"
+  type        = map(string)
+  default     = {}
+}
+variable "subnet" {
+  description = "Zones to launch our instances into"
+  type        = string
+  default     = ""
+}
+variable "volume_size" {
+  description = "volume size"
+  type        = number
+  default     = 8 
+}
+
+variable "ami_id" {
+  description = "Name of Launch configuration"
+  type        = string
+  default     = ""
+}
+variable "key_name" {
+  description = "Key name of Launch configuration"
+  type        = string
+  default     = ""
+}
+variable "instance_type" {
+  description = "Name of Launch configuration"
+  type        = string
+  default     = ""
+}
+variable "security_groups" {
+  description = "Name of Launch configuration"
+  type        = list(string)
+  default     = []
+}
+
+variable "iam_instance_profile" {
+  type = string
+  description = "IAM role to attach with ec2 instance"
+  default = ""
+}
+
+variable "kms_key_id" {
+  type = string
+  description = "insert KMS key"
+  default = ""
+}
+
+variable "volume_encryption" {
+  type = bool
+  description = "(Optional) Whether to enable volume encryption. Defaults to false."
+  default = false
+}
+
+variable "volume_type" {
+  type = string
+  description = "(Optional) Type of volume. Valid values include standard, gp2, gp3, io1, io2, sc1, or st1. Defaults to gp2"
+  default = "gp3"
+}
+
+variable "disable_api_termination" {
+  type = bool
+  default = true
+}
+
+variable "disable_api_stop" {
+  type = bool
+  default = true
+}
